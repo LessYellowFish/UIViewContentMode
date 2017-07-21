928fb13a893a0368369653e3a3ffe345579ef171
LessYellowFish <2601958675@qq.com>
2017-07-21 15:56:48 +0800

    G

---
+++ UICONTENTVIEW/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICONTENTVIEW
+//
+//  Created by 刘浩宇 on 17/7/21.
+//  Copyright © 2017年 GG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
